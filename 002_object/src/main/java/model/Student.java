22b0b2f935b21ec27e463771b6469c9cdbcd0305
RicoTech666 <zhoujiachenbe@gmail.com>
2020-02-07 17:12:23 +0800

    [Jiachen] feat: make student class

---
+++ 002_object/src/main/java/model/Student.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Student {
+    private String name;
+    private String age;
+    private String gender;
+
+    public Student() {
+    }
+
+    public Student(String name, String age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void study() {
+        System.out.println("我在学习");
+    }
+
+    public void rest() {
+        System.out.println("我在休息");
+    }
+}
