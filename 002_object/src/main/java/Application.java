ec3f1820beb103cbb6f46148d5aaf8190bb0cb25
RicoTech666 <zhoujiachenbe@gmail.com>
2020-02-07 17:21:31 +0800

    [Jiachen] feat: try student class

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,6 +1,11 @@
+import model.Student;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        Student stu = new Student("李莉","20岁","女");
+        System.out.println(stu.getName()+","+stu.getAge()+","+stu.getGender());
+        stu.study();
+        stu.rest();
     }
 }
